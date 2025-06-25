ec78e747e434549681fdb65390ec0065894fd299
ritty65 <contact@simulacraproductions.net>
2025-06-25 09:13:40 +0200

    fix: restore i18n legacy mode

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -7,7 +7,7 @@ const storedLocale =
   localStorage.getItem("cashu.language") || navigator.language || "en-US";
 
 export const i18n = createI18n({
-  legacy: false,
+  legacy: true,
   locale: storedLocale,
   fallbackLocale: "en-US",
   globalInjection: true,
