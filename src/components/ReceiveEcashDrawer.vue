f1819205cacbcc2b0f298aa52103f106feb6b785
ritty65 <contact@simulacraproductions.net>
2025-06-25 08:11:42 +0200

    Fix NDK relay set init and remove misplaced state mappings

--- src/components/ReceiveEcashDrawer.vue
+++ src/components/ReceiveEcashDrawer.vue
@@ -209,10 +209,6 @@ export default defineComponent({
       "toggleScanner",
       "pasteToParseDialog",
     ]),
-    ...mapWritableState(useReceiveTokensStore, [
-      "showReceiveTokens",
-      "receiveData",
-    ]),
     isiOsSafari() {
       const userAgent = window.navigator.userAgent.toLowerCase();
       const match =
